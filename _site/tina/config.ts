d4e87415680105caef0c0d32cb1ae38caec918dd
A <A>
2026-06-07 09:02:04 +0100

    the global config is now working

--- _site/tina/config.ts
+++ _site/tina/config.ts
@@ -12,6 +12,118 @@ const PageCollection = {
   ],
 };
 
+export const GlobalConfigCollection: Collection = {
+  name: "config",
+  label: "Global Config",
+  path: "_data",
+  format: "yml",
+  ui: {
+    global: true,
+  },
+  fields: [
+    {
+      name: "seo",
+      label: "General site config",
+      type: "object",
+      fields: [
+        {
+          name: "title",
+          label: "Site title for SEO",
+          type: "string",
+          required: true,
+        },
+        {
+          name: "description",
+          label: "Site description for SEO",
+          type: "string",
+          required: true,
+        },
+        {
+          name: "siteOwner",
+          label: "Your Name, Company Name (Used in the footer",
+          required: true,
+          type: "string",
+          ui: {
+            defaultValue: "Your name here"
+          },
+        },
+        {
+          name: 'logo',
+          label: 'Logo',
+          type: 'image',
+        }
+        //Add more site settings here...
+      ],
+    },
+    {
+      name: "nav",
+      label: "Site Navigation Menu (Reorder, Add, Remove)",
+      type: "object",
+      list: true,
+      ui: {
+        itemProps: (item) => {
+          return {
+            label: item.title
+          };
+        },
+      },
+      fields: [
+        {
+          name: "title",
+          label: "Title of Nav Item",
+          type: "string",
+          required: true
+        },
+        {
+          name: "link",
+          label: "Path of the Nav Item",
+          type: "string",
+          required: true
+
+        }
+      ]
+    },
+    {
+      name: "contactLinks",
+      label: "Contact Links",
+      type: "object",
+      list: true,
+      ui: {
+        itemProps: (item) => {
+          return {
+            label: item.title
+          }
+        },
+      },
+      fields: [
+        {
+          name: "title",
+          label: "Title",
+          type: "string"
+        },
+        {
+          name: "link",
+          label: "Link",
+          type: "string"
+        },
+        {
+          name: "icon",
+          label: "Icon",
+          description: "Any Tabler icon name, e.g. tabler:brand-x, tabler:book-2, tabler:brand-github. Browse at https://icones.js.org/collection/tabler",
+          type: "string"
+        }
+      ],
+    },
+    {
+      name: "footerStarfield",
+      label: "Show starfield in footer",
+      type: "boolean",
+    }
+
+    // Add other config fields here...
+  ]
+}
+
 export default defineConfig({
   branch: "main",
   clientId: null,              // local development – no cloud
@@ -34,6 +146,6 @@ export default defineConfig({
   },
 
   schema: {
-    collections: [PageCollection],
+    collections: [PageCollection, GlobalConfigCollection]
   },
 });
